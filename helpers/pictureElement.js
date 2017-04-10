b01abd7b760d8bb1a55e11117981ced91f792b9c
Phil Whittaker <phil@hifi.agency>
2017-04-10 15:53:00 +0100

    adding picture element and backrgound helpers

---
+++ helpers/pictureElement.js
@@ -0,0 +1,14 @@
+var Handlebars = require('handlebars'),
+	picture = require('./pictureHelpers.js');
+
+module.exports = function (src, alt, classes, fallbackSize) {	
+
+	// Collect extra params (breakpoints)
+	// Length - 1 as last param is a HBS object
+	var args = Array.prototype.slice.call(arguments),
+		breakpointArgs = args.slice(4, args.length -1);
+
+	var sources = picture.buildSourcesObj(breakpointArgs);		
+	return picture.element(src, alt, classes, sources, fallbackSize);
+};
+
