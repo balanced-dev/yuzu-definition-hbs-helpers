33b052690f3146b8c4894a5f93892268d623f736
Phil Whittaker <phil@hifi.agency>
2017-08-24 15:43:59 +0100

    added picture element with focal point

---
+++ helpers/pictureElementFocus.js
@@ -0,0 +1,13 @@
+var Handlebars = require('handlebars'),
+picture = require('./pictureHelpers.js');
+
+module.exports = function (src, alt, focalPoint, classes, fallbackSize) {	
+
+// Collect extra params (breakpoints)
+// Length - 1 as last param is a HBS object
+var args = Array.prototype.slice.call(arguments),
+	breakpointArgs = args.slice(5, args.length -1);
+
+var sources = picture.buildSourcesObj(breakpointArgs);		
+return picture.element(src, alt, focalPoint, classes, sources, fallbackSize);
+};
