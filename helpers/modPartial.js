91a140735f11571695c4eef85eded8f83ce4ee24
Lewis Farrow <lewis@hifi.agency>
2019-07-22 16:39:14 +0100

    Use arguments to create array of modifiers (complete array cannot be passed via parameter)

--- helpers/modPartial.js
+++ helpers/modPartial.js
@@ -1,9 +1,11 @@
-module.exports = function (path, context, modifiers) {
+module.exports = function (path, context) {
 	var partial = handlebars.partials[path];
 	if (typeof partial !== 'function') {
 		partial = handlebars.compile(partial);
 	}
-	if(context)
-		context['_modifiers'] = modifiers;
+	if(context) {
+		// Assume every parameter after path + context are modifier classes (excluding last = options object)
+		context._modifiers = Array.prototype.slice.call(arguments,2,-1);
+	}
 	return partial(context);
 };
