1b5deae445e94749b9af9be9c473aa4523536581
Phil Whittaker <phil@hifi.agency>
2020-08-22 11:27:28 +0100

    staticPartial

---
+++ helpers/staticPartial.js
@@ -0,0 +1,15 @@
+module.exports = function (path, context, scope) {
+	path = path.replace('/','');
+	var partial = handlebars.partials[path];
+
+	if(scope) {
+		if(context._endpoint) {
+			partial = partial.replace(/<\w*\s/, '$&data-endpoint=\''+ context._endpoint +'\' ');
+		}
+		else if (context) {
+			partial = partial.replace(/<\w*\s/, '$&data-context=\''+ JSON.stringify(context) +'\' ');
+		}
+	}
+
+	return partial;
+};
