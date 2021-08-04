cde9cc6eaf58f0e22cc7d553af805c95dfca05a3
Phil Whittaker <phil@hifi.agency>
2021-08-04 15:21:12 +0100

    improve static partial

--- helpers/staticPartial.js
+++ helpers/staticPartial.js
@@ -2,12 +2,17 @@ module.exports = function (path, context, scope) {
 	path = path.replace('/','');
 	var partial = handlebars.markup[path];
 
-	if(scope) {
-		if(context._endpoint) {
-			partial = partial.replace(/<\w*\s/, '$&data-endpoint=\''+ context._endpoint +'\' ');
-		}
-		else if (context) {
-			partial = partial.replace(/<\w*\s/, '$&data-context=\''+ JSON.stringify(context) +'\' ');
+	if(!partial) {
+		throw new Error(`Static Partial ${path} not found in static partial hbs helper`);
+	}
+	else {
+		if(scope) {
+			if(context._endpoint) {
+				partial = partial.replace(/<\w*\s/, '$&data-endpoint=\''+ context._endpoint +'\' ');
+			}
+			else if (context) {
+				partial = partial.replace(/<\w*\s/, '$&data-context=\''+ JSON.stringify(context) +'\' ');
+			}
 		}
 	}
 
